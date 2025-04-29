5714b20178df74502a31d992cfd26ce2285a425e
Kamal Sunar <kkskgamer@gmail.com>
2025-04-29 20:24:24 +0545

    setup complete

---
+++ makefile
@@ -0,0 +1,26 @@
+# Makefile for SDL3 Game Project with modular directories
+
+# Compiler and flags
+CC = gcc
+CFLAGS = -Wall -Wextra -std=c17 -Isrc -Iinclude $(shell pkg-config --cflags sdl3)
+LDFLAGS = $(shell pkg-config --libs sdl3)
+
+# Directories
+SRC_DIR = src
+INC_DIR = include
+
+# Source files
+SRC = $(SRC_DIR)/main.c
+
+# Output binary
+TARGET = main
+
+# Default target
+all: $(TARGET)
+
+$(TARGET): $(SRC)
+	$(CC) -o $@ $^ $(CFLAGS) $(LDFLAGS)
+
+# Clean target
+clean:
+	rm -f $(TARGET)
---
+++ src/main.c
@@ -0,0 +1,53 @@
+#define SDL_MAIN_USE_CALLBACKS 1
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+#define WIN_WIDTH 1000
+#define WIN_HEIGHT 625
+
+static SDL_Window *window = NULL;
+static SDL_Renderer *renderer = NULL;
+
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
+    (void)appstate;
+    (void)argc;
+    (void)argv;
+    SDL_SetAppMetadata("Kinetik", "1.0", "kinetik.com");
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Couldn't initilize SDL: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    if (!SDL_CreateWindowAndRenderer("Kinetic", WIN_WIDTH, WIN_HEIGHT, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    
+    return SDL_APP_CONTINUE;
+}
+
+
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
+    (void)appstate;
+    if (event->type == SDL_EVENT_QUIT) {
+        return SDL_APP_SUCCESS; // end the program, reporting success to the os
+    }
+
+    return SDL_APP_CONTINUE;
+}
+
+
+SDL_AppResult SDL_AppIterate(void *appstate) {
+    (void)appstate;
+
+    return SDL_APP_CONTINUE;
+}
+
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result) {
+    (void)appstate;
+    (void)result;
+    // SDL will clean up the window/renderer for us
+}
